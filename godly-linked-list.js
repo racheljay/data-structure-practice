f8915adada1311ec2321c32882d2a68dd3bf171d
Rachel <racheljay42@gmail.com>
2022-08-09 15:28:42 -0400

    refactor and add merge function

--- godly-linked-list.js
+++ godly-linked-list.js
@@ -29,7 +29,7 @@ class Node {
             // return this.string
             return dataString
         }
-        
+
     }
 }
 
@@ -76,7 +76,7 @@ class LinkedList {
     }
 
     appendArray(arr) {
-        for(let item of arr) {
+        for (let item of arr) {
             this.append(item)
         }
         return
@@ -94,29 +94,43 @@ class LinkedList {
             this.head = new Node(data)
             return
         }
-        if(this.head.next === null) {
-            if(this.head.data <= data) {
-                this.head.next = new Node(data)
-                return
-            } else if(this.head.data > data){
-                let oldHead = this.head
-                let newHead = new Node(data)
-                newHead.next = oldHead
-                this.head = newHead
-                return
-            }
-        }
+  
         let current = this.head
-        
-        while(current.next.next !== null) {
-            let oldNext = current.next.next
-            if (data <= current.next.next.data) {
-                current.next.next = new Node(data)
-                current.next.next.next = oldNext
+
+        while (current.next !== null) {
+
+            if (current.data <= data && current.next.data >= data) {
+                let oldNext = current.next
+                current.next = new Node(data)
+                current.next.next = oldNext
                 return
             }
+
             current = current.next
         }
+        if (this.head.data <= data) {
+            this.head.next = new Node(data)
+            return
+        } else {
+            let oldHead = this.head
+            let newHead = new Node(data)
+            newHead.next = oldHead
+            this.head = newHead
+            return
+        }
+    }
+
+    mergeOrderedList(list) {
+        if(list.head === null) {
+            return
+        }
+
+        let current = list.head
+        while (list.next != null) {
+            this.insertInOrder(current.data)
+            current = current.next
+        }
+        this.insertInOrder(current.data)
     }
 
     stringify() {
@@ -137,7 +151,7 @@ class LinkedList {
     stringifyTwo() {
         if (this.head === null) {
             console.log("this shit empty")
-            return 
+            return
         }
         console.log(this.head.stringifyTwo())
         return this.head.stringifyTwo()
@@ -155,39 +169,66 @@ class LinkedList {
 {
     const list = new LinkedList()
     const array = [34, 68, 24, 89, 5, 77]
-    
+
     // list.append(5)
     // list.append(7)
     // list.append(87)
     // list.append(33)
-    
+
     // list.appendArray(array)
-    
+
     // list.stringifyTwo()
-    
+
     // console.log(list.head)
-    
+
     // console.log(LinkedList.fromArray(array).stringify())
 }
 
 // Tests for insertInOrder
 
+// {
+//     const list = new LinkedList()
+//     list.appendArray([1, 2, 3, 5])
+
+//     list.insertInOrder(4)
+
+//     list.stringify()
+
+// }
+
+// {
+//     const list = new LinkedList()
+//     list.appendArray([7])
+
+//     list.insertInOrder(4)
+
+//     list.stringify()
+
+// }
+
+// {
+//     const list = new LinkedList()
+//     list.appendArray([2])
+
+//     list.insertInOrder(4)
+
+//     list.stringify()
+
+// }
+
+// {
+//     const list = new LinkedList()
+//     list.appendArray([1,3])
+//     list.insertInOrder(2)
+//     list.stringify()
+// }
+
 {
+    const otherList = new LinkedList()
+    otherList.appendArray([1, 5, 8])
+
     const list = new LinkedList()
-    list.appendArray([1, 2, 3, 5])
-
-    list.insertInOrder(4)
-
+    list.appendArray([2, 4, 6, 7])
+    list.mergeOrderedList(otherList)
     list.stringify()
-
-}
-
-{
-    const list = new LinkedList()
-    list.appendArray([7])
-
-    list.insertInOrder(4)
-
-    list.stringify()
-
 }
