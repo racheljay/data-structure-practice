b6eb534df9c9755e34abdde9f9186a43500b4285
Rachel <racheljay42@gmail.com>
2022-08-17 15:02:31 -0400

    queue push and pop

--- js-examples/queue.js
+++ js-examples/queue.js
@@ -7,13 +7,21 @@ class Queue extends LinkedList {
     }
 
     // add a new tail node
-    push() {
-
+    push(data) {
+        if (this.head === null) {
+            this.head = new Node(data)
+            this.tail = this.head
+            return
+        }
+        let oldTail = this.tail
+        let newTail = new Node(data)
+        oldTail.next = newTail
+        this.tail = newTail
     }
 
     // removes the current head node
     pop() {
-        if(this.head === null) {
+        if (this.head === null) {
             return
         }
         let newHead = this.head.next
@@ -23,6 +31,17 @@ class Queue extends LinkedList {
 
 let queue = new Queue()
 
-queue.append(4)
-queue.append(7)
+queue.push(2)
+queue.push(5)
+queue.push(12)
+queue.push(13)
+
+// queue.append(4)
+// queue.append(7)
+// queue.append(9)
+// queue.pop()
+
+console.log("head:", queue.head.data, "tail:", queue.tail.data)
+// console.log(queue.head)
+
 queue.stringify()
